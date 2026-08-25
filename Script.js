96decfca780ec4cab7cd0b8e0dfa51bccd5d086f
xicen124-art <xicen124@gmail.com>
2026-08-25 09:17:50 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded',function () {
  document.getElemtById('year').innerHTML=new Date().getFullYear();
    console.log('Portfolio ของ SOMCHAI พร้อมแล้ว!');
-}};
+});
